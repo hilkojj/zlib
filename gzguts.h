47c7b828152388d3efbbcf08eecc049854aa432e
hilkojj <hilkojj@outlook.com>
2020-04-19 23:42:54 +0200

    fixed 'implicit declaration of function ... is invalid in C99' when compiling with emscripten

--- gzguts.h
+++ gzguts.h
@@ -2,6 +2,9 @@
  * Copyright (C) 2004, 2005, 2010, 2011, 2012, 2013, 2016 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
+#ifdef EMSCRIPTEN
+#include <unistd.h>
+#endif
 
 #ifdef _LARGEFILE64_SOURCE
 #  ifndef _LARGEFILE_SOURCE
